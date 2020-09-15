75ecc6992a492e100b1a74640ba24d6c0643413f
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-15 11:53:16 +0530

    Total Working Hours Clause Cannot Be Passed

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,11 +1,11 @@
 public class EmpWageComputation
 {
-	static final int HALFDAY=1,FULLDAY=2,WAGEPERHR=20,MONTH=20;
+	static final int HALFDAY=1,FULLDAY=2,WAGEPERHR=20,MONTH=20,MAXWORKINGHRS=100;
 	public static void main(String[] args)
 	{
 
-		int workingHrs=0,salary,totalSalary=0;
-		for(int day=1;day<=MONTH;day++)
+		int workingHrs=0,salary,totalSalary=0,totalWorkingHrs=0,day;
+		for(day=1;day<=MONTH && totalWorkingHrs<=MAXWORKINGHRS;day++)
 		{
 			int random=(int)Math.floor(Math.random()*10)%3;
 
@@ -21,10 +21,19 @@ public class EmpWageComputation
 								workingHrs=0;
 			}
 
+			if(totalWorkingHrs==96||totalWorkingHrs==94)
+			{
+				if (workingHrs==8)
+				break;
+			}
+			else if(totalWorkingHrs==98)
+			{
+				break;
+			}
+			totalWorkingHrs+=workingHrs;
 			salary=WAGEPERHR*workingHrs;
 			totalSalary+=salary;
 		}
-
 
 		System.out.println("Employee Earns "+totalSalary+" This Month");
 
