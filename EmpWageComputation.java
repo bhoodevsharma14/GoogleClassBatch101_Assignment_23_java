ef6f39ba781880db412f52e1362c2138ef440c53
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-15 10:32:15 +0530

    Computing Salay

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,21 @@
 public class EmpWageComputation
 {
-	static final int PRESENT=1;
+	static final int PRESENT=1,WAGEPERHR=20;
 	public static void main(String[] args)
 	{
 		int random=(int)Math.floor(Math.random()*10)%2;
+		int workingHrs=0,salary;
 
 		if(PRESENT==random)
+		{
 			System.out.println("Employee Is Present ");
+			workingHrs=8;
+		}
 		else
 			System.out.println("Employee Is Absent ");
+		salary=WAGEPERHR*workingHrs;
+
+		System.out.println("Employee Earns "+salary);
 
 	}
 }
